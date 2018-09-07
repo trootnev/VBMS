826c01ecfa78dae818ac89f8c6d6f49be4021b02
trootnev <trootnev@hotmail.com>
2018-09-07 12:17:55 +0300

    adfsadf

--- vbms/VBMS 1.7/dbo/Tables/DbVersion.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[DbVersion] (
-    [Id]            INT           IDENTITY (1, 1) NOT NULL,
-    [Version]       NVARCHAR (20) NOT NULL,
-    [DateInstalled] DATETIME      NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
